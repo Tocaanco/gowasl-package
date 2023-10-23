e75e4562b63a10a094f725ff1f308052eb72d1ac
AhmedNabil94 <mody998866@gmail.com>
2023-10-23 20:39:48 +0200

    files

--- config/wa_integration.php
+++ config/wa_integration.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'id'            => env('WA_CHANNEL_ID','YOUR_CHANNEL_ID_HEER'),
-    'token'         => env('WA_CHANNEL_TOKEN','YOUR_CHANNEL_TOKEN_HEER'),
-    'identifier'    => env('WA_CHANNEL_IDENTIFIER','YOUR_IDENTIFIER_HEER'),
+    'id'            => env('WA_CHANNEL_ID','YOUR_CHANNEL_ID_HERE'),
+    'token'         => env('WA_CHANNEL_TOKEN','YOUR_CHANNEL_TOKEN_HERE'),
+    'identifier'    => env('WA_CHANNEL_IDENTIFIER','YOUR_IDENTIFIER_HERE'),
 ];
