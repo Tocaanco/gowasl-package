9b5d2cba77d90b268c56467bce356db082d00dd0
AhmedNabil94 <mody998866@gmail.com>
2023-11-19 00:22:02 +0200

    new message types

--- src/Services/MessageService.php
+++ src/Services/MessageService.php
@@ -12,4 +12,56 @@ class MessageService implements MessageContract
     {
         return (new WAconnection())->startRequest($this->base.'/sendMessage','POST',$data);
     }
+
+    public function sendImage($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendImage','POST',$data);
+    }
+
+    public function sendVideo($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendVideo','POST',$data);
+    }
+
+    public function sendAudio($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendAudio','POST',$data);
+    }
+
+    public function sendFile($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendFile','POST',$data);
+    }
+
+    public function sendLink($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendLink','POST',$data);
+    }
+
+    public function sendSticker($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendSticker','POST',$data);
+    }
+
+    public function sendGif($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendGif','POST',$data);
+    }
+
+    public function sendLocation($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendLocation','POST',$data);
+    }
+
+    public function sendContact($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendContact','POST',$data);
+    }
+
+    public function sendMention($data)
+    {
+        return (new WAconnection())->startRequest($this->base.'/sendMention','POST',$data);
+    }
+
+
 }
